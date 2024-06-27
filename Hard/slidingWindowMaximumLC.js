1a93295649ac838e4f332322ff8eb59e682683a7
Adrian Rodriguez-Cruz <98348151+Saydfalls@users.noreply.github.com>
2024-06-27 13:23:30 -0400

    I lied lolsies
    I had to go check everyone else's solutions and then I saw that there was one that was the same as mine, EXCEPT theirs simplified my if statement into for loop. I guess I should've seen that coming because it's that situation I took notes on where the condition exists outside of the loop and inside the loop. The conditional itself becomes redundant, so what this does is it moves the conditional to a while loop that performs the if statement until it can't which is great! That's a good way of thinking of while loops, and maybe loops in general, that being "iterative if statements". Should've been something I was already doing, but with practice it'll solidify more. I'm just a lil jelly doin his best to become diamond, what can I say \/(^-^)\/

--- Hard/slidingWindowMaximumLC.js
+++ Hard/slidingWindowMaximumLC.js
@@ -8,10 +8,8 @@ var maxSlidingWindow = function(nums, k) {
     let deque = [];
 
     for(let i = 0; i < nums.length; i++){
-        if(deque.length > 0 && nums[i] > nums[deque[deque.length - 1]]) {
-            for(let j = deque.length - 1; j >= 0; j--){
-                if(nums[i] > nums[deque[j]]) deque.splice(j, 1);
-            }
+        while (deque.length > 0 && nums[deque[deque.length - 1]] <= nums[i]) {
+            deque.pop();
         }
         deque.push(i);
         if(deque[0] < (i - k + 1)) deque.shift();
