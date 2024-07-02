d54fb77c0581eec332defd25dbecd13709eeabe4
Adrian Rodriguez-Cruz <98348151+Saydfalls@users.noreply.github.com>
2024-07-02 17:01:59 -0400

    I'm understanding the easy problems a lot better
    I'm getting a lot faster at solving easy problems. Though it may make my ability to complete medium problems easy, I'm not completing them more quickly. Until I've done enough medium problems, confidence will continue to affect my speed.

---
+++ Easy/canPlaceFlowersLC.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+
+    for(let i = 0; i < flowerbed.length; i++){
+        if(flowerbed[i] == 0 && flowerbed[i - 1] != 1 && flowerbed[i + 1] != 1) {
+            count++;
+            flowerbed[i] = 1;
+        }
+    }
+
+    return count >= n ? true : false;
+};
