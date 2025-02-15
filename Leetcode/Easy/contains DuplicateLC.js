0e1dafe92d012deb21f1aa1e186d21f50118217f
Adrian Rodriguez-Cruz <98348151+Saydfalls@users.noreply.github.com>
2025-02-15 12:29:53 -0500

    Fix: sport mode (performance upgrade ;] )
    I revisited this problem and realized I had come up with a different solution. Upon comparison, I recognized that my new solution is faster because it terminates early when finding a duplicate whereas the old solution is more elegant and readable.

--- Leetcode/Easy/contains DuplicateLC.js
+++ Leetcode/Easy/contains DuplicateLC.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+    let frequencyMap = new Set();
+
+    for(value of nums) {
+        if(frequencyMap.has(value)) return true;
+        frequencyMap.add(value);
+    }
+
+    return false;
+};
+
+// Old solution
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+/*
+var containsDuplicate = function(nums) {
     return new Set(nums).size != nums.length;
 };
+*/
