5efb050eefd33c178362a68c8cc79eb2ef48fdc8
Adrian Rodriguez-Cruz <98348151+Saydfalls@users.noreply.github.com>
2025-02-28 16:45:07 -0500

    Fix: binaryTreeInorderTraversalLC.js
    Everything is the same, aside from how nodes are checked and returning early on an empty tree. Also added the iterative solution. Reminder that in order traversal is done by checking left -> root -> right.

--- Leetcode/Easy/binaryTreeInorderTraversalLC.js
+++ Leetcode/Easy/binaryTreeInorderTraversalLC.js
@@ -11,6 +11,50 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
+    if(!root) return [];
+    let arr = [];
+
+    function ioT (node, arr)
+    {
+        if (!node) return;
+
+        ioT(node.left, arr);
+        arr.push(node.val);
+        ioT(node.right, arr);
+    }
+
+    ioT(root, arr);    
+    return arr;
+};
+
+/* ITERATIVE SOLUTION */
+/*
+var inorderTraversal = function(root) {
+    if(!root) return [];
+    let arr = [];
+    
+    let stack = [];
+    let current = root;
+    while (stack.length > 0 || current) 
+    {
+        while (current)
+        {
+            stack.push(current);
+            current = current.left;
+        }
+
+        current = stack.pop();
+        arr.push(current.val);
+        current = current.right;
+    }   
+    
+    return arr;
+};
+*/
+
+/* OLD SOLUTION */
+/*
+var inorderTraversal = function(root) {
     let arr = [];
 
     const recurseTraverse = function(currNode){
@@ -24,4 +68,4 @@ var inorderTraversal = function(root) {
     
     return arr;
 };
-
+*/
